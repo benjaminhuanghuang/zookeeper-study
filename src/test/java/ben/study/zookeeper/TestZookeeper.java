02c8c658017f2cc66531c2cccc860834e3dfeaf8
Benjamin Huang <bhuang@rms.com>
2019-12-15 19:31:43 -0800

    update

--- src/test/java/ben/study/zookeeper/TestZookeeper.java
+++ src/test/java/ben/study/zookeeper/TestZookeeper.java
@@ -1,13 +1,44 @@
 package ben.study.zookeeper;
 
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TestZookeeperTest {
+class TestZookeeper {
+    //    private String connectString = "ip:2181,ip:2181,ip:2181";
+    private String connectString = "server.1:2181";
+    private int sessionTimeout = 2000;
+    ZooKeeper zkClient;
+
+    @BeforeEach
+    public void initEach() throws IOException {
+        zkClient = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+
+            }
+        });
+    }
+
     @Test
-    public void init(){
+    public void createNode() throws IOException, KeeperException, InterruptedException {
+        String path = zkClient.create("/ben", "testdata".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+    }
 
+    @Test
+    public void getDataAndWatch() throws KeeperException, InterruptedException {
+        List<String> children = zkClient.getChildren("/", false);
 
+    }
+
+    @Test
+    public void isNodeExist() throws KeeperException, InterruptedException {
+        Stat stat = zkClient.exists("/test", false);
     }
 }
