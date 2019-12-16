6ddbebbbd6b60a586dc1496fd8e0fcc0ac1a6835
Benjamin Huang <bhuang@rms.com>
2019-12-15 19:30:32 -0800

    update

---
+++ docker/start-cluster.sh
@@ -0,0 +1,1 @@
+docker-compose -f ./docker-compose-zk-cluster.yml up
---
+++ src/test/java/ben/study/zookeeper/TestZookeeper.java
@@ -0,0 +1,13 @@
+package ben.study.zookeeper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestZookeeperTest {
+    @Test
+    public void init(){
+
+
+    }
+}
